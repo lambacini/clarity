e1456eeaea41c46e1e6c85f34a790659abca4866
Eudes Petonnet-Vincent <epetonnetvince@vmware.com>
2017-02-24 10:39:24 -0500

    [NG] Datagrid: popover prototype
    The body being able to scroll clipped row action menus. We fix this with a prototype of a more general popover solution.
    
    This dirties the RowActionService slightly. The components are more and more concerned with styling because it is done in Javascript, and we don't have a great way around it yet.
    
    Signed-off-by: Eudes Petonnet-Vincent <epetonnetvince@vmware.com>

--- src/clarity-angular/datagrid/providers/row-action-service.ts
+++ src/clarity-angular/datagrid/providers/row-action-service.ts
@@ -4,9 +4,13 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import {Injectable} from "@angular/core";
+import {ColumnsWidth} from "./columns-width";
 
 @Injectable()
 export class RowActionService {
+
+    constructor(private columnsWidth: ColumnsWidth) {}
+
     /**
      * a value of 0 means no rows with action
      */
@@ -23,6 +27,31 @@ export class RowActionService {
         this._actionableCount--;
     }
 
+    /*
+     * Ad-hoc dirty lock, handling only a single pending action
+     */
+    private locked = false;
+    private waiting: () => void;
 
+    public open(fn: () => void) {
+        if (!this.locked) {
+            this.locked = true;
+            fn();
+            // Scrollbar might have disappeared, we need to recompute the layout.
+            this.columnsWidth.setColumnsWidth();
+        } else {
+            this.waiting = fn;
+        }
+    }
 
+    public close() {
+        if (this.waiting) {
+            this.waiting();
+            delete this.waiting;
+        } else {
+            this.locked = false;
+            // Scrollbar might have appeared, we need to recompute the layout.
+            this.columnsWidth.setColumnsWidth();
+        }
+    }
 }
